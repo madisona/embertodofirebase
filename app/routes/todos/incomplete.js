49a2642fed28aefc498dee28b945a6de0c7959f6
Aaron Madison <aaron.l.madison@gmail.com>
2016-05-26 15:55:37 -0500

    Style: removed comments

--- app/routes/todos/incomplete.js
+++ app/routes/todos/incomplete.js
@@ -6,11 +6,6 @@ export default Ember.Route.extend({
       orderBy: 'complete',
       equalTo: false
     });
-    // below works to limit to only not complete items, but it seems to lose its firebase binding
-    //return this.store.filter('todo', {orderBy: 'complete', equalTo: false}, function(todo) {
-    //  console.log(todo.title + " " - todo.complete);
-    //  return !todo.get('complete');
-    //});
   },
   renderTemplate(controller, model) {
     this.render('todos.index', {
