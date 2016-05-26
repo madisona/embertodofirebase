82f11f7dbc93caeb6284d9e59e57a623bf59029a
Aaron Madison <aaron.l.madison@gmail.com>
2016-05-26 15:56:30 -0500

    removed didInsertElement

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -16,19 +16,12 @@ export default Ember.Component.extend({
   hasCompleted: Ember.computed('completed', function() {
     return this.get('completed') > 0;
   }),
-  didInsertElement() {
-    let todos = this.get('todos');
-    if (todos.get('length') > 0 && todos.isEvery('complete', true)) {
-      this.set('allAreDone', true);
-    } else {
-      this.set('allAreDone', false);
-    }
-  },
+
   allAreDoneObserver: Ember.observer('allAreDone', function() {
     let completeValue = this.get('allAreDone');
     let todos = this.get('todos');
     todos.forEach((todo) => {
-      todo.set('complete', completeValue)
+      todo.set('complete', completeValue);
       this.sendAction('updateTodo', todo);
     });
   }),
